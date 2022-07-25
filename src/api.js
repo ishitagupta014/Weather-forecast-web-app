bf9c3545630f9f4e2c36dcd00a914215746f0811
Ishita Gupta <ishitagupta014@gmail.com>
2022-07-26 02:28:08 +0530

    Weather Forecast app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9d3fc909b6msh86d8871f385c358p1204f2jsn3415d21fef5d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "58d83c4b725408b7e32a75fde8f023f6"; // enter your key from openweather API
